4df3192faad693bb6a2a5283c2956e0d32795a44
Andrew Neidley <neidz44@gmail.com>
2017-07-27 09:18:16 -0500

    Initialize and Authorize: in progress

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
 
   def initialize(username, password)
     @base_api_url = 'https://www.bloc.io/api/v1'
-    @auth_token = self.class.post('https://www.bloc.io/api/v1/sessions', username, password)
+    @auth_token = self.class.post('https://www.bloc.io/api/v1/sessions', {username: username, password: password} )
   end
 
 end
