5cda3e866dd99f2f280d432e67ebc428a8dbedf3
Andrew Neidley <neidz44@gmail.com>
2017-07-27 10:46:40 -0500

    Retrieve Users in progress

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,4 +8,10 @@ class Kele
     @response = Kele.post("#{base_url}/sessions", body: {email: email, password: password})
   end
 
+  def get_me
+    base_url = 'https://www.bloc.io/api/v1'
+    response = self.class.get("#{base_url}/sessions", headers: { "authorization" => @auth_token } )
+    JSON.parse(response)
+  end
+
 end
